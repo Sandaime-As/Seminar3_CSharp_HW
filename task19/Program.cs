2cababe9dc8363fa1e653a710e44e199d4e0c97e
Sandaime-As <asenov1998@mail.ru>
2022-09-29 23:00:20 +0600

    task19 and task23

--- task19/Program.cs
+++ task19/Program.cs
@@ -4,3 +4,12 @@
 23432 -> да*/
 using static System.Console;
 Clear();
+void Palindrom(int n){
+    string num = Convert.ToString(n);
+    if (num[0] == num[4] & num[1] == num[3]) WriteLine("Да");
+    else WriteLine("Нет");
+}
+Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(ReadLine()!);
+if (num <= 99999 & num >= 10000) Palindrom(num);
+else WriteLine("Неверные данные");
--- task23/Program.cs
+++ task23/Program.cs
@@ -4,3 +4,15 @@
 5 -> 1, 8, 27, 64, 125*/
 using static System.Console;
 Clear();
+void Cube(int num){
+    int index = 1;
+    while(index<=num)
+    {
+        Write(index*index*index + ", ");
+        index++;
+    }
+}
+Write("Введите число: ");
+int num = Convert.ToInt32(ReadLine()!);
+if (num <= 0) Write("Неверные данные");
+else Cube(num);
